00fd7356504fae16e6fac2cad6278f81d7385ab1
Paul <s017871@students.lmsd.org>
2024-03-08 07:45:17 -0500

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Paul Stricker (student LM) on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
