af45ee6b5e3394e5fb719e50971d594bc7632141
Jadyn Hart <s036596@students.lmsd.org>
2024-04-03 09:55:47 -0400

    tab view and stuff of that nature

---
+++ views/Top Stories.swift
@@ -0,0 +1,20 @@
+//
+//  Top Stories.swift
+//  FinalProject
+//
+//  Created by Jadyn Hart (student LM) on 4/2/24.
+//
+
+import SwiftUI
+
+struct Top_Stories: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Top_Stories_Previews: PreviewProvider {
+    static var previews: some View {
+        Top_Stories()
+    }
+}
