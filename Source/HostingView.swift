ed85dda4622dba3c2a4ce104ada38bec5985d4c7
David Oprea <75183102+DavidOprea@users.noreply.github.com>
2024-05-09 07:22:23 -0400

    Create HostingView.swift

---
+++ Source/HostingView.swift
@@ -0,0 +1,29 @@
+import UIKit
+import SwiftUI
+
+class HostingView<T: View>: UIView {
+
+    private(set) var hostingController: UIHostingController<T>
+
+    var rootView: T {
+        get { hostingController.rootView }
+        set { hostingController.rootView = newValue }
+    }
+
+    init(rootView: T, frame: CGRect = .zero) {
+        hostingController = UIHostingController(rootView: rootView)
+
+        super.init(frame: frame)
+
+        backgroundColor = .clear
+        hostingController.view.backgroundColor = backgroundColor
+        hostingController.view.frame = self.bounds
+        hostingController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        addSubview(hostingController.view)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
